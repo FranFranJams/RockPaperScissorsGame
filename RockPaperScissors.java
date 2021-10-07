40f11d2aca7b0f7df67308ea680ea9bf2adeba79
FranFranJams <SuperNewComputer16@outlook.com>
2021-10-07 22:09:18 +0100

    Finished RockPaperScissors game
    Added appropriate comments, added restart feature, added an Ascii intro.

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -4,11 +4,17 @@
 
 import java.util.Scanner;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+
 
 class RockPaperScissors
 {
-		public static void main(String[] args) 
-		{
+		public static void main(String[] args) throws InterruptedException 
+		{ // "throws InterruptedException" prevents the While loop from erroring the code.
+			while (true)
+{
+			@SuppressWarnings("resource") // This prevents Visual studio from giving a "Resource Leak" warning.
 			Scanner sc = new Scanner(System.in); 
 			Random rand = new Random();
 			int num;
@@ -17,12 +23,33 @@ class RockPaperScissors
 
 			
 			// Intro to the game
-			System.out.println("Welcome to Rock, Paper, Scissors");
-			System.out.print("Please choose from the following:\nR)ock, P)aper or S)cissors. > ");
-			
+
+			System.out.println(" _          _    _       __             			");
+			System.out.println("|_) _  _|  |_)_ |_)_ __ (_  _o _ _ _ ___			");
+			System.out.println("| \\(_)(_|< | (_|| (/_|  __)(_|_>_>(_)|_>			");
+			System.out.println(" ");
+			TimeUnit.MILLISECONDS.sleep(500);
+			System.out.println("...loading ");
+			TimeUnit.MILLISECONDS.sleep(100);
+			System.out.println(" ");
+			TimeUnit.MILLISECONDS.sleep(800);
+			System.out.println("Tweaking the code...");
+			TimeUnit.MILLISECONDS.sleep(700);
+			System.out.println("Making sure cheating is impossible...");
+			TimeUnit.MILLISECONDS.sleep(900);
+			System.out.println("Being gay.. I mean cleaning the CPU.....");
+			TimeUnit.MILLISECONDS.sleep(1000);
+			System.out.println("Starting...");
+
+
+			//System.out.println("Welcome to Rock, Paper, Scissors");
+			TimeUnit.MILLISECONDS.sleep(400);
+			System.out.println("Please choose from the following:");
+			TimeUnit.MILLISECONDS.sleep(1500);
+			System.out.print("R)ock, P)aper or S)cissors. > ");
 			// Expecting an input from user
 			userChoice = sc.nextLine();
-			sc.close();
+			//sc.close();
 			// CPU select random choice
 			num = rand.nextInt(3);
 			
@@ -88,6 +115,42 @@ class RockPaperScissors
 			{
 				System.out.println("It looks like a Tie! Better luck next time <3");
 			}
+
+			/* Ask user if they want to play again
+			* If yes, restarts game
+			* If no, thanks user for playing and exits after a few seconds
+			*/
+
+			System.out.print("Would you like to play again? (Y/N) > ");
+			userChoice = sc.nextLine();
+
+			if(userChoice.equals("Y"))
+			{
+				// Restarts game
+				System.out.println("Restarting...");
+				TimeUnit.MILLISECONDS.sleep(500);
+				System.out.println("... ");
+				TimeUnit.MILLISECONDS.sleep(500);
+				System.out.println("...");
+				TimeUnit.MILLISECONDS.sleep(500);
+				System.out.println("...");
+				TimeUnit.MILLISECONDS.sleep(500);
+				System.out.println("...");
+				TimeUnit.MILLISECONDS.sleep(3000);
+
+			}
+			else if(userChoice.equals("N")){
+				// Exits game
+				System.out.println("Exiting... thank you for playing <3");
+				TimeUnit.MILLISECONDS.sleep(500);
+				System.out.println("This game was created by FranFranJams (Fran[They/She]#7787 on discord)");
+				TimeUnit.MILLISECONDS.sleep(2500);
+
+				break; // Breaks the While Loop
+			}
+		
+		}
+		
 	}
 }
 
